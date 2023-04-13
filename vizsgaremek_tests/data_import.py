ef571075d830904de45c5287477454e0ff0cdd95
breni124 <69256717+breni124@users.noreply.github.com>
2023-04-13 13:55:29 +0200

    first tests

---
+++ vizsgaremek_tests/data_import.py
@@ -0,0 +1,20 @@
+
+user_data = {
+    'username': 'tesztelo0124',
+    'email': 'tesztelo0124@gmail.com',
+    'password': 'Tesztelek2023',
+}
+
+article = {
+    "title": "My first article",
+    "about": "My first lorem ipsum article",
+    "article": "This is my first lorem ipsum article.",
+    "tags": "lorem" "ipsum"
+        }
+
+modified_article = {
+    "title": "My modified first article",
+    "about": "Test for my modified first article",
+    "article": "This is my modified first lorem ipsum article",
+    "tags": "lorem" "ipsum"
+}
